4c9d05e6e61c3ab42df4d7d62def3ee28865f390
JohnReid <johnbaronreid@netscape.net>
2018-09-24 10:12:55 +0100

    Refactor scripts.

--- R/SIMLR.R
+++ R/SIMLR.R
@@ -356,3 +356,61 @@
     }
     return(result)
 }
+
+
+#' Apply the SIMLR algorithm to a data set and summarise the results
+#'
+apply_SIMLR <- function(.data, data_set, output_dir = file.path('output', data_set)) {
+  #
+  # run SIMLR
+  message("Running SIMLR")
+  res = SIMLR(X = .data$in_X, c = .data$n_clust, return_intermediaries = TRUE)
+
+  #
+  # Calculate NMI
+  nmi_1 = igraph::compare(.data$true_labs[,1], res$y$cluster, method = "nmi")
+
+  #
+  # Report results
+  print('Iterations:')
+  print(res$iter)
+  print(res$execution.time)
+  print('Convergence:')
+  print(res$converge)
+  print('Weights:')
+  print(res$alphaK)
+
+  #
+  # Scatter plot of dimensionality reduction
+  output_file <- purrr::partial(file.path, output_dir)
+  dir.create(output_file('.'), recursive = TRUE, showWarnings = FALSE)
+  pdf(output_file('scatter.pdf'), width=9, height=6, paper='special')
+  plot(res$ydata,
+      col = get_palette(3)[.data$true_labs[,1]],
+      xlab = "SIMLR component 1",
+      ylab = "SIMLR component 2",
+      pch = 20,
+      main = "SIMILR 2D visualization")
+  dev.off()
+
+
+  #
+  # Make a heatmap of S
+  # devtools::load_all('../..')
+  pdf(output_file('heatmap.pdf'), width=8, height=8, paper='special')
+  similarity.heatmap(res$S,
+                    label = stringr::str_c('label ', .data$true_labs[,1]),
+                    cluster = stringr::str_c('cluster ', res$y$cluster))
+  dev.off()
+
+  #
+  # Show and save timings
+  print(res$timings)
+  readr::write_csv(res$timings %>% dplyr::mutate(data.set = data_set, niter = res$iter), output_file('timings.csv'))
+
+  #
+  # Show NMI
+  message('NMI: ', nmi_1)
+
+  return(res)
+}
--- inst/example_scripts/Buettner.R
+++ inst/example_scripts/Buettner.R
@@ -5,10 +5,6 @@
 #
 
 #
-# Load packages
-library(tidyverse)
-
-#
 # load the SIMLR R package
 # devtools::document('../..')
 # devtools::build('../..')
@@ -16,57 +12,6 @@ devtools::load_all('../..')
 # ls('package:SIMLR')
 
 #
-# Configuration
-.data <- Buettner
-output_file <- purrr::partial(file.path, 'output', 'Buettner')
-dir.create(output_file('.'), recursive = TRUE, showWarnings = FALSE)
-
-#
-# run SIMLR
+# Run SIMLR
 set.seed(11111)
-message("Running SIMLR")
-res = SIMLR(X = .data$in_X, c = .data$n_clust, return_intermediaries = TRUE)
-
-#
-# Calculate NMI
-nmi_1 = igraph::compare(.data$true_labs[,1], res$y$cluster, method = "nmi")
-
-#
-# Report results
-print('Iterations:')
-print(res$iter)
-print(res$execution.time)
-print('Convergence:')
-print(res$converge)
-print('Weights:')
-print(res$alphaK)
-
-#
-# Scatter plot of dimensionality reduction
-pdf(output_file('scatter.pdf'), width=9, height=6, paper='special')
-plot(res$ydata,
-     col = get_palette(3)[.data$true_labs[,1]],
-     xlab = "SIMLR component 1",
-     ylab = "SIMLR component 2",
-     pch = 20,
-     main = "SIMILR 2D visualization")
-dev.off()
-
-
-#
-# Make a heatmap of S
-# devtools::load_all('../..')
-pdf(output_file('heatmap.pdf'), width=8, height=8, paper='special')
-similarity.heatmap(res$S,
-                   label = str_c('label ', .data$true_labs[,1]),
-                   cluster = str_c('cluster ', res$y$cluster))
-dev.off()
-
-#
-# Show and save timings
-print(res$timings)
-readr::write_csv(res$timings, output_file('timings.csv'))
-
-#
-# Show NMI
-message('NMI: ', nmi_1)
+res <- apply_SIMLR(Buettner, 'Buettner')
