0ae0d3bb1594c9fce19d2785884f2ba6cf315aea
JohnReid <johnbaronreid@netscape.net>
2018-10-12 09:32:32 +0100

    Format with styler

--- R/utils.simlr.R
+++ R/utils.simlr.R
@@ -4,33 +4,37 @@
 #'
 #' @keywords internal
 #'
-CumulativeTimer <- R6::R6Class("CumulativeTimer",
-  public = list(
-    last_time = NULL,
-    timings = list(),
-    initialize = function() {
-      self$reset_last_time()
-    },
-    reset_last_time = function() { self$last_time <- proc.time() },
-    add = function(name) {
-      # How long since the last call?
-      elapsed = proc.time() - self$last_time
-      # If we already have an elapsed time for the name, add it
-      if( name %in% names(self$timings) ) {
-        elapsed = self$timings[[name]] + elapsed
+CumulativeTimer <-
+  R6::R6Class(
+    "CumulativeTimer",
+    public = list(
+      last_time = NULL,
+      timings = list(),
+      initialize = function() {
+        self$reset_last_time()
+      },
+      reset_last_time = function() {
+        self$last_time <- proc.time()
+      },
+      add = function(name) {
+        # How long since the last call?
+        elapsed <- proc.time() - self$last_time
+        # If we already have an elapsed time for the name, add it
+        if (name %in% names(self$timings)) {
+          elapsed <- self$timings[[name]] + elapsed
+        }
+        # Assign to our list
+        self$timings[[name]] <- elapsed
+        # Update the last time
+        self$reset_last_time()
+      },
+      get_timings = function() {
+        as.data.frame(t(as.data.frame(lapply(self$timings, data.matrix)))) %>%
+          dplyr::mutate(task = names(self$timings)) %>%
+          dplyr::arrange(-elapsed)
       }
-      # Assign to our list
-      self$timings[[name]] = elapsed
-      # Update the last time
-      self$reset_last_time()
-    },
-    get_timings = function() {
-      as.data.frame(t(as.data.frame(lapply(self$timings, data.matrix)))) %>%
-        dplyr::mutate(task = names(self$timings)) %>%
-        dplyr::arrange(-elapsed)
-    }
+    )
   )
-)
 
 
 #' Compute the eigenvalues and eigenvectors of A
@@ -42,50 +46,49 @@ CumulativeTimer <- R6::R6Class("CumulativeTimer",
 #'
 #' @keywords internal
 #'
-"eig1" <- function( A, c = NA, isMax = NA, isSym = NA ) {
+eig1 <- function(A, c = NA, isMax = NA, isSym = NA) {
 
-    # set the needed parameters
-    if(is.na(c)) {
-        c = dim(A)[1]
-    }
-    if(c>dim(A)[1]) {
-        c = dim(A)[1]
-    }
-    if(is.na(isMax)) {
-        isMax = 1
-    }
-    if(is.na(isSym)) {
-        isSym = 1
-    }
+  # set the needed parameters
+  if (is.na(c)) {
+    c <- dim(A)[1]
+  }
+  if (c > dim(A)[1]) {
+    c <- dim(A)[1]
+  }
+  if (is.na(isMax)) {
+    isMax <- 1
+  }
+  if (is.na(isSym)) {
+    isSym <- 1
+  }
 
-    # compute the eigenvalues and eigenvectors of A
-    if(isSym==1) {
-        eigen_A = eigen(A,symmetric=TRUE)
-    }
-    else {
-        eigen_A = eigen(A)
-    }
-    v = eigen_A$vectors
-    d = eigen_A$values
+  # compute the eigenvalues and eigenvectors of A
+  if (isSym == 1) {
+    eigen_A <- eigen(A, symmetric = TRUE)
+  }
+  else {
+    eigen_A <- eigen(A)
+  }
+  v <- eigen_A$vectors
+  d <- eigen_A$values
 
-    # sort the eigenvectors
-    if(isMax == 0) {
-        eigen_A_sorted = sort(d,index.return=TRUE)
-    }
-    else {
-        eigen_A_sorted = sort(d,decreasing=TRUE,index.return=TRUE)
-    }
-    d1 = eigen_A_sorted$x
-    idx = eigen_A_sorted$ix
-    idx1 = idx[1:c]
+  # sort the eigenvectors
+  if (isMax == 0) {
+    eigen_A_sorted <- sort(d, index.return = TRUE)
+  }
+  else {
+    eigen_A_sorted <- sort(d, decreasing = TRUE, index.return = TRUE)
+  }
+  d1 <- eigen_A_sorted$x
+  idx <- eigen_A_sorted$ix
+  idx1 <- idx[1:c]
 
-    # compute the results
-    eigval = d[idx1]
-    eigvec = Re(v[,idx1])
-    eigval_full = d[idx]
+  # compute the results
+  eigval <- d[idx1]
+  eigvec <- Re(v[, idx1])
+  eigval_full <- d[idx]
 
-    return(list(eigval=eigval, eigvec=eigvec, eigval_full=eigval_full))
-
+  return(list(eigval = eigval, eigvec = eigvec, eigval_full = eigval_full))
 }
 
 
@@ -98,33 +101,33 @@ CumulativeTimer <- R6::R6Class("CumulativeTimer",
 #'
 #' @keywords internal
 #'
-"L2_distance_1" <- function( a, b = NULL ) {
-    # Add a row of zeros to a if it only has one row
-    if(dim(a)[1] == 1) {
-        a = rbind(a, rep(0, dim(a)[2]))
+L2_distance_1 <- function(a, b = NULL) {
+  # Add a row of zeros to a if it only has one row
+  if (dim(a)[1] == 1) {
+    a <- rbind(a, rep(0, dim(a)[2]))
+  }
+  # Take L2 norm of each column
+  aa <- apply(a, MARGIN = 2, FUN = function(x) sum(x^2))
+  # If b is given then use it, otherwise use calculation of a
+  if (is.null(b)) {
+    b <- a
+    bb <- aa
+  } else {
+    # Add a row of zeros to b if it only has one row
+    if (dim(b)[1] == 1) {
+      b <- rbind(b, rep(0, dim(b)[2]))
     }
     # Take L2 norm of each column
-    aa = apply(a, MARGIN=2, FUN=function(x) sum(x^2))
-    # If b is given then use it, otherwise use calculation of a
-    if( is.null(b) ) {
-        b = a
-        bb = aa
-    } else {
-        # Add a row of zeros to b if it only has one row
-        if(dim(b)[1] == 1) {
-            b = rbind(b, rep(0, dim(b)[2]))
-        }
-        # Take L2 norm of each column
-        bb = apply(b, MARGIN=2, FUN=function(x) sum(x^2))
-    }
-    # Calculate distances
-    d = Re(outer(aa, bb, FUN = '+') - 2 * crossprod(a, b))
-    # Make sure all distances are non-negative
-    d[d < 0] = 0
-    # Make sure diagonal is exactly 0
-    diag(d) = 0
-    #
-    return(d)
+    bb <- apply(b, MARGIN = 2, FUN = function(x) sum(x^2))
+  }
+  # Calculate distances
+  d <- Re(outer(aa, bb, FUN = "+") - 2 * crossprod(a, b))
+  # Make sure all distances are non-negative
+  d[d < 0] <- 0
+  # Make sure diagonal is exactly 0
+  diag(d) <- 0
+  #
+  return(d)
 }
 
 
@@ -134,67 +137,66 @@ CumulativeTimer <- R6::R6Class("CumulativeTimer",
 #'
 #' @keywords internal
 #'
-umkl = function( D, beta = NA ) {
+umkl <- function(D, beta = NA) {
 
-    # set some parameters
-    if(is.na(beta)) {
-        beta = 1 / length(D)
+  # set some parameters
+  if (is.na(beta)) {
+    beta <- 1 / length(D)
+  }
+  tol <- 1e-4
+  u <- 20
+  logU <- log(u)
+
+  # compute Hbeta
+  res_hbeta <- Hbeta(D, beta)
+  H <- res_hbeta$H
+  thisP <- res_hbeta$P
+
+  betamin <- -Inf
+  betamax <- Inf
+  # evaluate whether the perplexity is within tolerance
+  Hdiff <- H - logU
+  tries <- 0
+  while (abs(Hdiff) > tol && tries < 30) {
+    # if not, increase or decrease precision
+    if (Hdiff > 0) {
+      betamin <- beta
+      if (abs(betamax) == Inf) {
+        beta <- beta * 2
+      }
+      else {
+        beta <- (beta + betamax) / 2
+      }
     }
-    tol = 1e-4
-    u = 20
-    logU = log(u)
-
-    # compute Hbeta
-    res_hbeta = Hbeta(D, beta)
-    H = res_hbeta$H
-    thisP = res_hbeta$P
-
-    betamin = -Inf
-    betamax = Inf
-    # evaluate whether the perplexity is within tolerance
-    Hdiff = H - logU
-    tries = 0
-    while (abs(Hdiff) > tol && tries < 30) {
-        #if not, increase or decrease precision
-        if (Hdiff > 0) {
-            betamin = beta
-            if(abs(betamax)==Inf) {
-                beta = beta * 2
-            }
-            else {
-                beta = (beta + betamax) / 2
-            }
-        }
-        else {
-            betamax = beta
-            if(abs(betamin)==Inf) {
-                beta = beta * 2
-            }
-            else {
-                beta = (beta + betamin) / 2
-            }
-        }
-        # compute the new values
-        res_hbeta = Hbeta(D, beta)
-        H = res_hbeta$H
-        thisP = res_hbeta$P
-        Hdiff = H - logU
-        tries = tries + 1
+    else {
+      betamax <- beta
+      if (abs(betamin) == Inf) {
+        beta <- beta * 2
+      }
+      else {
+        beta <- (beta + betamin) / 2
+      }
     }
+    # compute the new values
+    res_hbeta <- Hbeta(D, beta)
+    H <- res_hbeta$H
+    thisP <- res_hbeta$P
+    Hdiff <- H - logU
+    tries <- tries + 1
+  }
 
-    return(thisP)
+  return(thisP)
 }
 
 
-Hbeta = function( D, beta ) {
+Hbeta <- function(D, beta) {
+  D <- (D - min(D)) / (max(D) - min(D) + .Machine$double.eps)
+  P <- exp(-D * beta)
+  sumP <- sum(P)
+  H <- log(sumP) + beta * sum(D * P) / sumP
+  P <- P / sumP
 
-    D = (D - min(D)) / (max(D) - min(D) + .Machine$double.eps)
-    P = exp(-D * beta)
-    sumP = sum(P)
-    H = log(sumP) + beta * sum(D * P) / sumP
-    P = P / sumP
-
-    return(list(H=H,P=P))
+  return(list(H = H, P = P))
 }
 
 
@@ -202,13 +204,13 @@ Hbeta = function( D, beta ) {
 #'
 #' @keywords internal
 #'
-sort.rows = function(X, cl, decreasing = FALSE) {
-  sorted = array(0, c(nrow(X), ncol(X)))
-  idx = array(0, c(nrow(X), ncol(X)))
-  for(i in 1:nrow(X)) {
-    res = sort(X[i,], index.return = TRUE, decreasing = decreasing)
-    sorted[i,] = res$x
-    idx[i,] = res$ix
+sort.rows <- function(X, cl, decreasing = FALSE) {
+  sorted <- array(0, c(nrow(X), ncol(X)))
+  idx <- array(0, c(nrow(X), ncol(X)))
+  for (i in 1:nrow(X)) {
+    res <- sort(X[i, ], index.return = TRUE, decreasing = decreasing)
+    sorted[i, ] <- res$x
+    idx[i, ] <- res$ix
   }
   return(list(sorted = sorted, idx = idx))
 }
@@ -231,7 +233,7 @@ zero_vec <- function(x, k) {
 #'
 #' @keywords internal
 #'
-order.rows = function(X, cl) t(parApply(cl, X, MARGIN = 1, FUN = order))
+order.rows <- function(X, cl) t(parApply(cl, X, MARGIN = 1, FUN = order))
 
 
 #' Return a vector of integers from from to to that are approximately evenly spaced
@@ -240,7 +242,7 @@ order.rows = function(X, cl) t(parApply(cl, X, MARGIN = 1, FUN = order))
 #'
 approx_spaced_integers <- function(from, to, max_length) {
   result <- from:to
-  if( ! is.null(max_length) && length(result) > max_length ) {
+  if (!is.null(max_length) && length(result) > max_length) {
     result <- round(seq(from, to, length.out = max_length))
   }
   return(result)
@@ -259,7 +261,8 @@ kernel_param_map <- function() {
   return(data.frame(
     kernel = 1:55,
     k = factor(rep(allk, each = length(sigma))),
-    sigma = factor(rep(sigma, length(allk)))))
+    sigma = factor(rep(sigma, length(allk)))
+  ))
 }
 
 
@@ -268,9 +271,9 @@ kernel_param_map <- function() {
 #' @keywords internal
 #'
 cores_from_ratio <- function(cores.ratio) {
-  cores = as.integer(cores.ratio * (detectCores() - 1))
+  cores <- as.integer(cores.ratio * (detectCores() - 1))
   if (cores < 1 || is.na(cores) || is.null(cores)) {
-    cores = 1
+    cores <- 1
   }
   return(cores)
 }
@@ -283,11 +286,13 @@ cores_from_ratio <- function(cores.ratio) {
 #' @keywords internal
 #'
 start_cluster <- function(num.cores = 0, cores.ratio = 1) {
-  if( ! num.cores ) {
+  if (!num.cores) {
     # Choose how many cores we will use for parallelisation
     num.cores <- cores_from_ratio(cores.ratio)
   }
-  cl = makeCluster(num.cores)
-  clusterEvalQ(cl, {library(Matrix)})
+  cl <- makeCluster(num.cores)
+  clusterEvalQ(cl, {
+    library(Matrix)
+  })
   return(cl)
 }
