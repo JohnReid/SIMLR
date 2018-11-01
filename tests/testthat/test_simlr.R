20aaeb79bcd37827a8543f27eb4549d9c1670049
JohnReid <johnbaronreid@netscape.net>
2018-11-01 17:14:32 +0000

    Update SIMLR test.

--- tests/testthat/test_simlr.R
+++ tests/testthat/test_simlr.R
@@ -1,34 +1,31 @@
+#
+# The names we expect to see in the results
+expected_names <- c('y', 'S', 'ydata', 'alphaK', 'execution.time')
+
+#
+# SIMLR
+context("SIMLR")
 normal <- SIMLR(X = BuettnerFlorian$in_X, c = BuettnerFlorian$n_clust, cores.ratio = 0)
 if.impute <- SIMLR(X = BuettnerFlorian$in_X, c = BuettnerFlorian$n_clust, cores.ratio = 0, if.impute = TRUE)
 normalise <- SIMLR(X = BuettnerFlorian$in_X, c = BuettnerFlorian$n_clust, cores.ratio = 0, normalize = TRUE)
-cimlr <- CIMLR(X = GliomasReduced$in_X, c = 3, cores.ratio = 0)
-ranks <- SIMLR_Feature_Ranking(A = BuettnerFlorian$results$S, X = BuettnerFlorian$in_X)
-
-context("SIMLR")
 test_that("structure of output is compliant", {
-  expect_equal(names(normal), c(
-    "y", "S", "F", "ydata",
-    "alphaK", "execution.time", "converge", "LF"
-  ))
-  expect_equal(names(if.impute), c(
-    "y", "S", "F", "ydata",
-    "alphaK", "execution.time", "converge", "LF"
-  ))
-  expect_equal(names(normalise), c(
-    "y", "S", "F", "ydata",
-    "alphaK", "execution.time", "converge", "LF"
-  ))
+  expect_true(all(expected_names %in% names(normal)))
+  expect_true(all(expected_names %in% names(if.impute)))
+  expect_true(all(expected_names %in% names(normalise)))
 })
 
+#
+# CIMLR
 context("CIMLR")
+cimlr <- CIMLR(X = GliomasReduced$in_X, c = 3, cores.ratio = 0)
 test_that("structure of output is compliant", {
-  expect_equal(names(cimlr), c(
-    "y", "S", "F", "ydata",
-    "alphaK", "execution.time", "converge", "LF"
-  ))
+  expect_true(all(expected_names %in% names(cimlr)))
 })
 
+#
+# Ranking
 context("SIMLR ranking")
+ranks <- SIMLR_Feature_Ranking(A = BuettnerFlorian$results$S, X = BuettnerFlorian$in_X)
 test_that("structure of output is compliant", {
   expect_equal(names(ranks), c("pval", "aggR"))
 })
