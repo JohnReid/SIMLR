5ffe7f2dc470b73f5f37fe9621fadde434e166e3
JohnReid <johnbaronreid@netscape.net>
2018-11-02 10:38:42 +0000

    Suppress warnings

--- tests/testthat/test_simlr.R
+++ tests/testthat/test_simlr.R
@@ -7,17 +7,17 @@ expected_names <- c('y', 'S', 'ydata', 'alphaK', 'execution.time')
 # SIMLR
 context("SIMLR")
 test_that("normal result names are compliant", {
-  normal <- SIMLR(X = BuettnerFlorian$in_X, c = BuettnerFlorian$n_clust, cores.ratio = 0)
+  suppressWarnings(normal <- SIMLR(X = BuettnerFlorian$in_X, c = BuettnerFlorian$n_clust, cores.ratio = 0))
   expect_true(all(expected_names %in% names(normal)))
 })
 
 test_that("imputed result names are compliant", {
-  if.impute <- SIMLR(X = BuettnerFlorian$in_X, c = BuettnerFlorian$n_clust, cores.ratio = 0, if.impute = TRUE)
+  suppressWarnings(if.impute <- SIMLR(X = BuettnerFlorian$in_X, c = BuettnerFlorian$n_clust, cores.ratio = 0, if.impute = TRUE))
   expect_true(all(expected_names %in% names(if.impute)))
 })
 
 test_that("normalized result names are compliant", {
-  normalise <- SIMLR(X = BuettnerFlorian$in_X, c = BuettnerFlorian$n_clust, cores.ratio = 0, normalize = TRUE)
+  suppressWarnings(normalise <- SIMLR(X = BuettnerFlorian$in_X, c = BuettnerFlorian$n_clust, cores.ratio = 0, normalize = TRUE))
   expect_true(all(expected_names %in% names(normalise)))
 })
 
@@ -26,7 +26,7 @@ test_that("normalized result names are compliant", {
 # CIMLR
 context("CIMLR")
 test_that("result names are compliant", {
-  cimlr <- CIMLR(X = GliomasReduced$in_X, c = 3, cores.ratio = 0)
+  suppressWarnings(cimlr <- CIMLR(X = GliomasReduced$in_X, c = 3, cores.ratio = 0))
   expect_true(all(expected_names %in% names(cimlr)))
 })
 
@@ -35,6 +35,6 @@ test_that("result names are compliant", {
 # Ranking
 context("SIMLR ranking")
 test_that("result names are compliant", {
-  ranks <- SIMLR_Feature_Ranking(A = BuettnerFlorian$results$S, X = BuettnerFlorian$in_X)
+  suppressWarnings(ranks <- SIMLR_Feature_Ranking(A = BuettnerFlorian$results$S, X = BuettnerFlorian$in_X))
   expect_equal(names(ranks), c("pval", "aggR"))
 })
