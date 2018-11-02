4c0780559b497b20d4ebe31ad658e7f9234b0b90
JohnReid <johnbaronreid@netscape.net>
2018-11-02 10:38:25 +0000

    Fix bracket typo

--- tests/testthat/test_simlr.R
+++ tests/testthat/test_simlr.R
@@ -9,12 +9,12 @@ context("SIMLR")
 test_that("normal result names are compliant", {
   normal <- SIMLR(X = BuettnerFlorian$in_X, c = BuettnerFlorian$n_clust, cores.ratio = 0)
   expect_true(all(expected_names %in% names(normal)))
-}
+})
 
 test_that("imputed result names are compliant", {
   if.impute <- SIMLR(X = BuettnerFlorian$in_X, c = BuettnerFlorian$n_clust, cores.ratio = 0, if.impute = TRUE)
   expect_true(all(expected_names %in% names(if.impute)))
-}
+})
 
 test_that("normalized result names are compliant", {
   normalise <- SIMLR(X = BuettnerFlorian$in_X, c = BuettnerFlorian$n_clust, cores.ratio = 0, normalize = TRUE)
