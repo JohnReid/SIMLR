9ba9aebc067953903c72686c86892c3432afeb4c
JohnReid <johnbaronreid@netscape.net>
2018-11-02 10:08:43 +0000

    Update data documentation

--- R/data.R
+++ R/data.R
@@ -6,13 +6,16 @@
 #' @format gene expression measurements of individual cells
 #' @source Buettner, Florian, et al. "Computational analysis of cell-to-cell heterogeneity in single-cell RNA-sequencing data reveals hidden subpopulations of cells." Nature biotechnology 33.2 (2015): 155-160.
 #' @return list of 6:
-#' 		in_X = input dataset as an (m x n) gene expression measurements of individual cells,
-#'  	n_clust = number of clusters (number of distinct true labels),
-#'  	true_labs = ground true of cluster assignments for each of the n_clust clusters,
-#'  	seed = seed used to compute the results for the example,
-#'  	results = result by SIMLR for the inputs defined as described,
-#'  	nmi = normalized mutual information as a measure of the inferred clusters compared to the true labels
+#'   \enumerate{
+#'      \item in_X = input dataset as an (m x n) gene expression measurements of individual cells,
+#'      \item n_clust = number of clusters (number of distinct true labels),
+#'      \item true_labs = ground true of cluster assignments for each of the n_clust clusters,
+#'      \item seed = seed used to compute the results for the example,
+#'      \item results = result by SIMLR for the inputs defined as described,
+#'      \item nmi = normalized mutual information as a measure of the inferred clusters compared to the true labels
+#'   }
 NULL
+
 
 #' @name ZeiselAmit
 #' @title test dataset for SIMLR large scale
@@ -22,13 +25,106 @@ NULL
 #' @format gene expression measurements of individual cells
 #' @source Zeisel, Amit, et al. "Cell types in the mouse cortex and hippocampus revealed by single-cell RNA-seq." Science 347.6226 (2015): 1138-1142.
 #' @return list of 6:
-#' 		in_X = input dataset as an (m x n) gene expression measurements of individual cells,
-#'  	n_clust = number of clusters (number of distinct true labels),
-#'  	true_labs = ground true of cluster assignments for each of the n_clust clusters,
-#'  	seed = seed used to compute the results for the example,
-#'  	results = result by SIMLR for the inputs defined as described,
-#'  	nmi = normalized mutual information as a measure of the inferred clusters compared to the true labels
+#'   \enumerate{
+#'     \item in_X = input dataset as an (m x n) gene expression measurements of individual cells,
+#'     \item n_clust = number of clusters (number of distinct true labels),
+#'     \item true_labs = ground true of cluster assignments for each of the n_clust clusters,
+#'     \item seed = seed used to compute the results for the example,
+#'     \item results = result by SIMLR for the inputs defined as described,
+#'     \item nmi = normalized mutual information as a measure of the inferred clusters compared to the true labels
+#'   }
 NULL
+
+
+#' @name Buettner
+#' @title Data from Buettner et al.
+#' @description Data from Buettner et al.
+#' @docType data
+#' @usage data(Buettner)
+#' @format gene expression measurements of individual cells
+#' @return list of 6:
+#'   \enumerate{
+#'     \item in_X = input dataset as an (m x n) gene expression measurements of individual cells,
+#'     \item n_clust = number of clusters (number of distinct true labels),
+#'     \item true_labs = ground true of cluster assignments for each of the n_clust clusters,
+#'     \item seed = seed used to compute the results for the example,
+#'     \item results = result by SIMLR for the inputs defined as described,
+#'     \item nmi = normalized mutual information as a measure of the inferred clusters compared to the true labels
+#'   }
+NULL
+
+
+#' @name Kolodziejczyk
+#' @title Data from Kolodziejczyk et al.
+#' @description Data from Kolodziejczyk et al.
+#' @docType data
+#' @usage data(Kolodziejczyk)
+#' @format gene expression measurements of individual cells
+#' @return list of 6:
+#'   \enumerate{
+#'     \item in_X = input dataset as an (m x n) gene expression measurements of individual cells,
+#'     \item n_clust = number of clusters (number of distinct true labels),
+#'     \item true_labs = ground true of cluster assignments for each of the n_clust clusters,
+#'     \item seed = seed used to compute the results for the example,
+#'     \item results = result by SIMLR for the inputs defined as described,
+#'     \item nmi = normalized mutual information as a measure of the inferred clusters compared to the true labels
+#'   }
+NULL
+
+
+#' @name Pollen
+#' @title Data from Pollen et al.
+#' @description Data from Pollen et al.
+#' @docType data
+#' @usage data(Pollen)
+#' @format gene expression measurements of individual cells
+#' @return list of 6:
+#'   \enumerate{
+#'     \item in_X = input dataset as an (m x n) gene expression measurements of individual cells,
+#'     \item n_clust = number of clusters (number of distinct true labels),
+#'     \item true_labs = ground true of cluster assignments for each of the n_clust clusters,
+#'     \item seed = seed used to compute the results for the example,
+#'     \item results = result by SIMLR for the inputs defined as described,
+#'     \item nmi = normalized mutual information as a measure of the inferred clusters compared to the true labels
+#'   }
+NULL
+
+
+#' @name Usoskin
+#' @title Data from Usoskin et al.
+#' @description Data from Usoskin et al.
+#' @docType data
+#' @usage data(Usoskin)
+#' @format gene expression measurements of individual cells
+#' @return list of 6:
+#'   \enumerate{
+#'     \item in_X = input dataset as an (m x n) gene expression measurements of individual cells,
+#'     \item n_clust = number of clusters (number of distinct true labels),
+#'     \item true_labs = ground true of cluster assignments for each of the n_clust clusters,
+#'     \item seed = seed used to compute the results for the example,
+#'     \item results = result by SIMLR for the inputs defined as described,
+#'     \item nmi = normalized mutual information as a measure of the inferred clusters compared to the true labels
+#'   }
+NULL
+
+
+#' @name Zeisel
+#' @title Data from Zeisel et al.
+#' @description Data from Zeisel et al.
+#' @docType data
+#' @usage data(Zeisel)
+#' @format gene expression measurements of individual cells
+#' @return list of 6:
+#'   \enumerate{
+#'     \item in_X = input dataset as an (m x n) gene expression measurements of individual cells,
+#'     \item n_clust = number of clusters (number of distinct true labels),
+#'     \item true_labs = ground true of cluster assignments for each of the n_clust clusters,
+#'     \item seed = seed used to compute the results for the example,
+#'     \item results = result by SIMLR for the inputs defined as described,
+#'     \item nmi = normalized mutual information as a measure of the inferred clusters compared to the true labels
+#'   }
+NULL
+
 
 #' @name GliomasReduced
 #' @title test dataset for CIMLR
@@ -38,5 +134,21 @@ NULL
 #' @format multi-omic data of cancer patients
 #' @source Cancer Genome Atlas Research Network. "Comprehensive, integrative genomic analysis of diffuse lower-grade gliomas." New England Journal of Medicine 372.26 (2015): 2481-2498.
 #' @return list of 1 element:
-#' 		in_X = input dataset as a list of 4 (reduced) multi-omic data each of which is an (m x n) measurements of cancer patients
+#'   \enumerate{
+#'     \item in_X = input dataset as a list of 4 (reduced) multi-omic data each of which is an (m x n) measurements of cancer patients
+#'   }
+NULL
+
+
+#' @name GliomasFull
+#' @title test dataset for CIMLR
+#' @description This is a dataset from the work by The Cancer Genome Atlas Research Network.
+#' @docType data
+#' @usage data(GliomasFull)
+#' @format multi-omic data of cancer patients
+#' @source Cancer Genome Atlas Research Network. "Comprehensive, integrative genomic analysis of diffuse lower-grade gliomas." New England Journal of Medicine 372.26 (2015): 2481-2498.
+#' @return list of 1 element:
+#'   \enumerate{
+#'     \item in_X = input dataset as a list of 4 multi-omic data each of which is an (m x n) measurements of cancer patients
+#'   }
 NULL
